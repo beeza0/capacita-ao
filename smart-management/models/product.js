0316d6fad529bb0bbcc7534271f815186ba6a9be
Bernardo <bernardoandrade@cpejr.com.br>
2019-06-28 10:42:42 -0300

    Projeto WEB

---
+++ smart-management/models/product.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+
+  name: String,
+  price: String
+
+}, { timestamps: true, static: false });
+
+const ProductModel = mongoose.model('Product', productSchema);
+
+class Product {
+  /**
+   * Get all Users from database
+   * @returns {Array} Array of Products
+   */
+  static getAll() {
+    return new Promise((resolve, reject) => {
+      ProductModel.find({}).exec().then((results) => {
+        resolve(results);
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  }
+
+  /**
+   * Get a User by it's id
+   * @param {string} id - User Id
+   * @returns {Object} - User Document Data
+   */
+  static getById(id) {
+    return new Promise((resolve, reject) => {
+      ProductModel.findById(id).exec().then((result) => {
+        resolve(result);
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  }
+
+  /**
+   * Create a new User
+   * @param {Object} user - User Document Data
+   * @returns {string} - New User Id
+   */
+  static create(user) {
+    return new Promise((resolve, reject) => {
+      ProductModel.create(user).then((result) => {
+        resolve(result._id);
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  }
+
+  /**
+   * Update a User
+   * @param {string} id - User Id
+   * @param {Object} User - User Document Data
+   * @returns {null}
+   */
+  static update(id, user) {
+    return new Promise((resolve, reject) => {
+      ProductModel.findByIdAndUpdate(id, user).then(() => {
+        resolve();
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  }
+
+  /**
+  * Delete a User
+  * @param {string} id - User Id
+  * @returns {null}
+  */
+
+  static delete(id) {
+    return new Promise((resolve, reject) => {
+      ProductModel.findOneAndDelete({_id: id}).then(() => {
+        resolve();
+      }).catch((err) => {
+        reject(err);
+      });
+   });
+ }
+}
+
+module.exports = Product;
